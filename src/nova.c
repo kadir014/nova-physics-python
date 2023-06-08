51dc4032fad497825f8aaa5400b9447008aa3d66
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-06-08 07:48:23 +0300

    one file wrapper

--- src/nova.c
+++ src/nova.c
@@ -1,16 +1,581 @@
-/*
-
-  This file is a part of the Python binding of the
-  Nova Physics Engine project and distributed under the MIT license.
-
-  Copyright © Kadir Aksoy
-  https://github.com/kadir014/nova-physics-python
-
-*/
-
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#include "api.h"
+#include "structmember.h"
+#include "novaphysics/novaphysics.h"
+
+
+
+/**
+ * Vector2 object interface
+ */
+typedef struct {
+    PyObject_HEAD
+    double x;
+    double y;
+} nv_Vector2Object;
+
+/**
+ * Create new Vector2 object
+ */
+nv_Vector2Object *nv_Vector2Object_new(double x, double y);
+
+/**
+ * Vector2 Python object to Vector2 struct
+ */
+#define PY_TO_VEC2(o) (NV_VEC2((o)->x, (o)->y))
+
+/**
+ * Body object interface
+ */
+typedef struct {
+    PyObject_HEAD
+    nv_Body *body;
+    nv_BodyType type;
+    int shape;
+    nv_Vector2Object *position;
+    double angle;
+    double radius;
+} nv_BodyObject;
+
+/**
+ * Space object interface
+ */
+typedef struct {
+    PyObject_HEAD
+    nv_Space *space;
+    nv_Array *body_objects;
+} nv_SpaceObject;
+
+
+
+static void nv_Vector2Object_dealloc(nv_Vector2Object *self) {
+    Py_TYPE(self)->tp_free((PyObject *) self);
+}
+
+static int nv_Vector2Object_init(
+    nv_Vector2Object *self,
+    PyObject *args,
+    PyObject *kwds
+) {
+    double x;
+    double y;
+
+    if (!PyArg_ParseTuple(args, "dd", &x, &y))
+        return -1;
+
+    self->x = x;
+    self->y = y;
+
+    return 0;
+}
+
+static PyObject *nv_Vector2Object___add__(
+    nv_Vector2Object *self,
+    nv_Vector2Object *vector
+);
+
+static PyObject *nv_Vector2Object___sub__(
+    nv_Vector2Object *self,
+    nv_Vector2Object *vector
+);
+
+static PyObject *nv_Vector2Object___mul__(
+    nv_Vector2Object *self,
+    PyObject *scalar
+);
+
+static PyObject *nv_Vector2Object___truediv__(
+    nv_Vector2Object *self,
+    PyObject *scalar
+);
+
+
+/**
+ * Vector2 object method interface
+ */
+static PyMethodDef nv_Vector2Object_methods[] = {
+    {
+        "__add__",
+        (PyCFunction)nv_Vector2Object___add__, METH_VARARGS,
+        ""
+    },
+
+    {NULL} // Sentinel
+};
+
+static PyObject *nv_Vector2Object___add__(
+    nv_Vector2Object *self,
+    nv_Vector2Object *vector
+) {
+    nv_Vector2 result = nv_Vector2_add(PY_TO_VEC2(self), PY_TO_VEC2(vector));
+
+    return nv_Vector2Object_new(result.x, result.y);
+}
+
+static PyObject *nv_Vector2Object___sub__(
+    nv_Vector2Object *self,
+    nv_Vector2Object *vector
+) {
+    nv_Vector2 result = nv_Vector2_sub(PY_TO_VEC2(self), PY_TO_VEC2(vector));
+
+    return nv_Vector2Object_new(result.x, result.y);
+}
+
+static PyObject *nv_Vector2Object___mul__(
+    nv_Vector2Object *self,
+    PyObject *scalar
+) {
+    double s = PyFloat_AS_DOUBLE(scalar);
+
+    nv_Vector2 result = nv_Vector2_muls(PY_TO_VEC2(self), s);
+
+    return nv_Vector2Object_new(result.x, result.y);
+}
+
+static PyObject *nv_Vector2Object___truediv__(
+    nv_Vector2Object *self,
+    PyObject *scalar
+) {
+    double s = PyFloat_AS_DOUBLE(scalar);
+
+    nv_Vector2 result = nv_Vector2_divs(PY_TO_VEC2(self), s);
+
+    return nv_Vector2Object_new(result.x, result.y);
+}
+
+/**
+ * Vector2 object operator overloadings
+ */
+static PyNumberMethods nv_Vector2Object_operators = {
+    .nb_add =         (binaryfunc)nv_Vector2Object___add__,
+    .nb_subtract =    (binaryfunc)nv_Vector2Object___sub__,
+    .nb_multiply =    (binaryfunc)nv_Vector2Object___mul__,
+    .nb_true_divide = (binaryfunc)nv_Vector2Object___truediv__
+};
+
+/**
+ * Vector2 object member interface
+ */
+static PyMemberDef nv_Vector2Object_members[] = {
+    {
+        "x",
+        T_DOUBLE, offsetof(nv_Vector2Object, x), 0,
+        "X component of the vector"
+    },
+
+    {
+        "y",
+        T_DOUBLE, offsetof(nv_Vector2Object, y), 0,
+        "Y component of the vector"
+    },
+
+    {NULL} // Sentinel
+};
+
+/**
+ * Vector2 object type internals
+ */
+PyTypeObject nv_Vector2ObjectType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "nova.Vector2",
+    .tp_doc = "Vector2 object",
+    .tp_basicsize = sizeof(nv_Vector2Object),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_new = PyType_GenericNew,
+    .tp_dealloc = (destructor)nv_Vector2Object_dealloc,
+    .tp_init = (initproc)nv_Vector2Object_init,
+    .tp_members = nv_Vector2Object_members,
+    .tp_as_number = &nv_Vector2Object_operators
+};
+
+nv_Vector2Object *nv_Vector2Object_new(double x, double y) {
+    PyObject *args = Py_BuildValue("dd", x, y);
+    nv_Vector2Object *obj = (nv_Vector2Object *)PyObject_CallObject((PyObject *)&nv_Vector2ObjectType, args);
+    Py_DECREF(args);
+    Py_INCREF(obj);
+    return obj;
+}
+
+
+
+static void nv_BodyObject_dealloc(nv_BodyObject *self) {
+    // Don't free nv_Body instance because space frees it
+    Py_XDECREF(self->position);
+    Py_TYPE(self)->tp_free((PyObject *) self);
+}
+
+static int nv_BodyObject_init(
+    nv_BodyObject *self,
+    PyObject *args,
+    PyObject *kwds
+) {
+    nv_BodyType type;
+    int shape;
+    double x;
+    double y;
+    double angle;
+    double radius;
+    PyObject *vertices = NULL;
+
+    if (!PyArg_ParseTuple(
+        args, "iidddd|O",
+        &type, &shape, &x, &y, &angle, &radius, &vertices
+    ))
+        return -1;
+
+    self->type = type;
+    self->shape = shape;
+    self->position = (nv_Vector2Object *)nv_Vector2Object_new(x, y);
+    self->angle = angle;
+    self->radius = radius;
+
+    nv_Array *new_vertices = NULL;
+
+    // Validate polygon vertices
+    if (vertices) {
+        if (!PySequence_Check(vertices)) {
+            PyErr_SetString(PyExc_TypeError, "Vertices must be a sequence of number pairs");
+            return 0;
+        }
+
+        size_t vertices_len = PySequence_Length(vertices);
+
+        if (vertices_len < 3) {
+            PyErr_SetString(PyExc_ValueError, "Polygon vertices must be at least length of 3");
+            return 0;
+        }
+
+        // Create nv_Array from vertices sequence
+        new_vertices = nv_Array_new();
+        PyObject *v;
+        PyObject *vx;
+        PyObject *vy;
+
+        for (size_t i = 0; i < vertices_len; i++) {
+            v = PySequence_GetItem(vertices, i);
+            vx = PySequence_GetItem(v, 0);
+            vy = PySequence_GetItem(v, 1);
+
+            nv_Array_add(new_vertices, NV_VEC2_NEW(PyFloat_AS_DOUBLE(vx), PyFloat_AS_DOUBLE(vy)));
+
+            Py_DECREF(v);
+            Py_DECREF(vx);
+            Py_DECREF(vy);
+        }
+    }
+
+    if (shape == 0) {
+        self->body = nv_Circle_new(
+            type,
+            NV_VEC2(x, y),
+            angle,
+            (nv_Material){1.0, 0.0, 0.0},
+            radius
+        );
+    }
+
+    else if (shape == 1) {
+        self->body = nv_Polygon_new(
+            type,
+            NV_VEC2(x, y),
+            angle,
+            (nv_Material){1.0, 0.0, 0.0},
+            new_vertices
+        );
+    }
+
+    return 0;
+}
+
+/**
+ * Body object member interface
+ */
+static PyMemberDef nv_BodyObject_members[] = {
+    {
+        "type",
+        T_INT, offsetof(nv_BodyObject, type), 0,
+        "Type of the body"
+    },
+
+    {
+        "shape",
+        T_INT, offsetof(nv_BodyObject, shape), 0,
+        "Shape of the body"
+    },
+
+    {
+        "position",
+        T_OBJECT_EX, offsetof(nv_BodyObject, position), 0,
+        "Position of the body"
+    },
+
+    {
+        "angle",
+        T_DOUBLE, offsetof(nv_BodyObject, angle), 0,
+        "Angle"
+    },
+
+    {
+        "radius",
+        T_DOUBLE, offsetof(nv_BodyObject, radius), 0,
+        "Radius"
+    },
+
+    {NULL} // Sentinel
+};
+
+static PyObject *nv_BodyObject_get_vertices(
+    nv_BodyObject *self,
+    PyObject *Py_UNUSED(ignored)
+) {
+    nv_Polygon_model_to_world(self->body);
+    PyObject *return_tup = PyTuple_New(self->body->shape->trans_vertices->size);
+
+    PyObject *vertex_tup;
+
+    for (size_t i = 0; i < self->body->shape->trans_vertices->size; i++) {
+        nv_Vector2 v = NV_TO_VEC2(self->body->shape->trans_vertices->data[i]);
+
+        PyTuple_SET_ITEM(return_tup, i, (PyObject *)nv_Vector2Object_new(v.x, v.y));
+    }
+
+    return return_tup;
+}
+
+static PyObject *nv_BodyObject_apply_force(
+    nv_BodyObject *self,
+    PyObject *args
+) {
+    nv_Vector2Object *force;
+
+    if (!PyArg_ParseTuple(args, "O!", &nv_Vector2ObjectType, &force))
+        return NULL;
+
+    nv_Body_apply_force(self->body, NV_VEC2(force->x, force->y));
+
+    Py_RETURN_NONE;
+}
+
+/**
+ * Body object method interface
+ */
+static PyMethodDef nv_BodyObject_methods[] = {
+    {
+        "get_vertices",
+        (PyCFunction)nv_BodyObject_get_vertices, METH_NOARGS,
+        "Get the transformed vertices"
+    },
+
+    {
+        "apply_force",
+        (PyCFunction)nv_BodyObject_apply_force, METH_VARARGS,
+        "Apply force at body center of mass"
+    },
+
+    {NULL} // Sentinel
+};
+
+PyTypeObject nv_BodyObjectType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "nova.Body",
+    .tp_doc = "Body object",
+    .tp_basicsize = sizeof(nv_BodyObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_new = PyType_GenericNew,
+    .tp_dealloc = (destructor)nv_BodyObject_dealloc,
+    .tp_init = (initproc)nv_BodyObject_init,
+    .tp_methods = nv_BodyObject_methods,
+    .tp_members = nv_BodyObject_members
+};
+
+
+PyObject *nv_create_circle(PyObject *self, PyObject *args) {
+    nv_BodyType type;
+    double x;
+    double y;
+    double angle;
+    double radius;
+
+    if (!PyArg_ParseTuple(
+        args, "idddd",
+        &type, &x, &y, &angle, &radius
+    ))
+        return NULL;
+
+    PyObject *inst_args = Py_BuildValue("iidddd", type, 0, x, y, angle, radius);
+    nv_BodyObject *obj = (nv_BodyObject *)PyObject_CallObject((PyObject *)&nv_BodyObjectType, inst_args);
+    Py_DECREF(inst_args);
+    return (PyObject *)obj;
+}
+
+PyObject *nv_create_rect(PyObject *self, PyObject *args) {
+    nv_BodyType type;
+    double x;
+    double y;
+    double angle;
+    double width;
+    double height;
+
+    if (!PyArg_ParseTuple(
+        args, "iddddd",
+        &type, &x, &y, &angle, &width, &height
+    ))
+        return NULL;
+
+    double w = width / 2.0;
+    double h = height / 2.0;
+
+    PyObject *inst_args = Py_BuildValue("iidddd((dd)(dd)(dd)(dd))", type, 1, x, y, angle, 0.0,
+        -w, -h, w, -h, w, h, -w, h);
+
+    nv_BodyObject *obj = (nv_BodyObject *)PyObject_CallObject((PyObject *)&nv_BodyObjectType, inst_args);
+    Py_DECREF(inst_args);
+    return (PyObject *)obj;
+}
+
+
+
+static void nv_SpaceObject_dealloc(nv_SpaceObject *self) {
+    nv_Space_free(self->space);
+
+    // Decrease reference of each body object in array
+    for (size_t i = 0; i < self->body_objects->size; i++) {
+        PyObject *body = (PyObject *)self->body_objects->data[i];
+        Py_DECREF(body);
+    }
+
+    nv_Array_free(self->body_objects);
+
+    Py_TYPE(self)->tp_free((PyObject *) self);
+}
+
+static int nv_SpaceObject_init(
+    nv_SpaceObject *self,
+    PyObject *args,
+    PyObject *kwds
+) {
+    self->space = nv_Space_new();
+    self->body_objects = nv_Array_new();
+
+    return 0;
+}
+
+
+static PyObject *nv_SpaceObject_step(
+    nv_SpaceObject *self,
+    PyObject *args
+) {
+    double dt;
+    int velocity_iters;
+    int position_iters;
+    int constraint_iters;
+    int substeps;
+
+    if (!PyArg_ParseTuple(
+            args, "diiii",
+            &dt,
+            &velocity_iters,
+            &position_iters,
+            &constraint_iters,
+            &substeps
+    )) return NULL;
+
+    nv_Space_step(
+        self->space,
+        dt,
+        velocity_iters,
+        position_iters,
+        constraint_iters,
+        substeps
+    );
+
+    // Update every body object
+    for (size_t i = 0; i < self->body_objects->size; i++) {
+        nv_Body *body = (nv_Body *)self->space->bodies->data[i];
+        nv_BodyObject *body_object = (nv_BodyObject *)self->body_objects->data[i];
+        Py_INCREF(body_object);
+        
+        body_object->position->x = body->position.x;
+        body_object->position->y = body->position.y; 
+        body_object->angle = body->angle; 
+        body_object->radius = body->shape->radius; 
+    }
+
+    Py_RETURN_NONE;
+}
+
+static PyObject *nv_SpaceObject_get_bodies(
+    nv_SpaceObject *self,
+    PyObject *Py_UNUSED(ignored)
+) {
+    PyObject *return_tup = PyTuple_New(self->body_objects->size);
+
+    for (size_t i = 0; i < self->body_objects->size; i++) {
+        nv_BodyObject *body = (nv_BodyObject *)self->body_objects->data[i];
+        
+        Py_INCREF(body);
+        PyTuple_SET_ITEM(return_tup, i, body);
+    }
+
+    return return_tup;
+}
+
+static PyObject *nv_SpaceObject_add(
+    nv_SpaceObject *self,
+    PyObject *args
+) {
+    nv_BodyObject *body;
+
+    if (!PyArg_ParseTuple(args, "O!", &nv_BodyObjectType, &body))
+        return NULL;
+
+    nv_Space_add(self->space, body->body);
+    Py_INCREF(body);
+    nv_Array_add(self->body_objects, body);
+
+    Py_RETURN_NONE;
+}
+
+/**
+ * Space object method interface
+ */
+static PyMethodDef nv_SpaceObject_methods[] = {
+    {
+        "step",
+        (PyCFunction)nv_SpaceObject_step, METH_VARARGS,
+        "Advance the simulation"
+    },
+
+    {
+        "get_bodies",
+        (PyCFunction)nv_SpaceObject_get_bodies, METH_NOARGS,
+        "Get bodies"
+    },
+
+    {
+        "add",
+        (PyCFunction)nv_SpaceObject_add, METH_VARARGS,
+        "Add body to space"
+    },
+
+    {NULL} // Sentinel
+};
+
+PyTypeObject nv_SpaceType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "nova.Space",
+    .tp_doc = "Space object",
+    .tp_basicsize = sizeof(nv_SpaceObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_new = PyType_GenericNew,
+    .tp_dealloc = (destructor)nv_SpaceObject_dealloc,
+    .tp_init = (initproc)nv_SpaceObject_init,
+    .tp_methods = nv_SpaceObject_methods
+};
+
 
 
 /**
