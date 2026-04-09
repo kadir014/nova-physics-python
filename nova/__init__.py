b8cc01f262ec7a42b7f5d353b27e5c0d1ec40450
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2026-04-09 13:28:20 +0300

    Improve constraints API
    - Implement all methods
    - Make constraint bodies read-only properties

--- nova/__init__.py
+++ nova/__init__.py
@@ -387,6 +387,7 @@ class Space:
 
     def add_constraint(self, constraint: Type["ConstraintT"]) -> None:
         constraint._refd = True
+        constraint._space = self
         ret = lib.nvSpace_add_constraint(self._space, constraint._cons)
         if ret == 2:
             raise DuplicateError("Can't add same constraint to same space more than once.")
@@ -396,6 +397,7 @@ class Space:
 
     def remove_constraint(self, constraint: Type["ConstraintT"]) -> None:
         constraint._refd = False
+        constraint._space = None
         if lib.nvSpace_remove_constraint(self._space, constraint._cons):
             raise NovaError(get_error_buffer())
         self._cons_ref.remove(constraint)
@@ -945,15 +947,32 @@ class RigidBody:
 
 
 class Constraint:
-    def __init__(self, a: RigidBody | None, b: RigidBody | None) -> None:
-        self.a = a
-        self.b = b
-        self._cons = None
+    def __init__(self, ccons) -> None:
+        self._cons = ccons
         self._refd = False
+
+        # This is only existent when constraint is managed by the space
+        # it is to access Python representations by pointers
+        self._space: Space | None = None
+
+    @property
+    def body_a(self) -> RigidBody | None:
+        if self._space is None:
+            return None
+        
+        cbody = self._cons.a
+        return self._space._get_body_by_pointer(cbody)
+    
+    @property
+    def body_b(self) -> RigidBody | None:
+        if self._space is None:
+            return None
+        
+        cbody = self._cons.b
+        return self._space._get_body_by_pointer(cbody)
 
     def __del__(self) -> None:
         if self._refd: return
-        self.a, self.b = None, None
         lib.nvConstraint_free(self._cons)
 
 ConstraintT = TypeVar("ConstraintT", bound=Constraint)
@@ -962,8 +981,8 @@ ConstraintT = TypeVar("ConstraintT", bound=Constraint)
 class DistanceConstraint(Constraint):
     def __init__(
             self,
-            a: RigidBody | None,
-            b: RigidBody | None,
+            body_a: RigidBody | None,
+            body_b: RigidBody | None,
             length: float,
             anchor_a: Vector2 = Vector2(0.0, 0.0),
             anchor_b: Vector2 = Vector2(0.0, 0.0),
@@ -971,18 +990,26 @@ class DistanceConstraint(Constraint):
             hertz: float = 3.0,
             damping: float = 0.3
         ) -> None:
-        super().__init__(a, b)
-
         init = lib.nvDistanceConstraintInitializer_default
-        init.a = a._rigidbody if a is not None else ffi.NULL
-        init.b = b._rigidbody if b is not None else ffi.NULL
+        init.a = body_a._rigidbody if body_a is not None else ffi.NULL
+        init.b = body_b._rigidbody if body_b is not None else ffi.NULL
         init.length = length
         init.anchor_a = anchor_a.to_tuple()
         init.anchor_b = anchor_b.to_tuple()
         init.spring = spring
         init.hertz = hertz
         init.damping = damping
-        self._cons = lib.nvDistanceConstraint_new(init)
+        ccons = lib.nvDistanceConstraint_new(init)
+
+        super().__init__(ccons)
+
+    @property
+    def length(self) -> float:
+        return lib.nvDistanceConstraint_get_length(self._cons)
+    
+    @length.setter
+    def length(self, length: float) -> None:
+        lib.nvDistanceConstraint_set_length(self._cons, length)
 
     @property
     def anchor_a(self) -> Vector2:
@@ -1002,22 +1029,56 @@ class DistanceConstraint(Constraint):
     def anchor_b(self, anchor_b: Vector2) -> None:
         lib.nvDistanceConstraint_set_anchor_b(self._cons, anchor_b.to_tuple())
 
+    @property
+    def max_force(self) -> float:
+        return lib.nvDistanceConstraint_get_max_force(self._cons)
+    
+    @max_force.setter
+    def max_force(self, max_force: float) -> None:
+        lib.nvDistanceConstraint_set_max_force(self._cons, max_force)
+
+    @property
+    def is_spring(self) -> bool:
+        return lib.nvDistanceConstraint_get_spring(self._cons)
+    
+    @is_spring.setter
+    def is_spring(self, is_spring: bool) -> None:
+        lib.nvDistanceConstraint_set_spring(self._cons, is_spring)
+
+    @property
+    def hertz(self) -> float:
+        return lib.nvDistanceConstraint_get_hertz(self._cons)
+    
+    @hertz.setter
+    def hertz(self, hertz: float) -> None:
+        lib.nvDistanceConstraint_set_hertz(self._cons, hertz)
+
+    @property
+    def damping(self) -> float:
+        return lib.nvDistanceConstraint_get_damping(self._cons)
+    
+    @damping.setter
+    def damping(self, damping: float) -> None:
+        lib.nvDistanceConstraint_set_damping(self._cons, damping)
+
 
 class HingeConstraint(Constraint):
     def __init__(
             self,
-            a: RigidBody,
-            b: RigidBody,
+            body_a: RigidBody,
+            body_b: RigidBody,
             anchor: Vector2,
             enable_limits: bool = False
         ) -> None:
-        super().__init__(a, b)
 
         init = lib.nvHingeConstraintInitializer_default
-        init.a = a._rigidbody if a is not None else ffi.NULL
-        init.b = b._rigidbody if b is not None else ffi.NULL
+        init.a = body_a._rigidbody if body_a is not None else ffi.NULL
+        init.b = body_b._rigidbody if body_b is not None else ffi.NULL
         init.anchor = anchor.to_tuple()
-        self._cons = lib.nvHingeConstraint_new(init)
+        init.enable_limits = enable_limits
+        ccons = lib.nvHingeConstraint_new(init)
+
+        super().__init__(ccons)
 
     @property
     def anchor(self) -> Vector2:
@@ -1027,3 +1088,35 @@ class HingeConstraint(Constraint):
     @anchor.setter
     def anchor(self, anchor: Vector2) -> None:
         lib.nvHingeConstraint_set_anchor(self._cons, anchor.to_tuple())
+
+    @property
+    def enable_limits(self) -> bool:
+        return lib.nvHingeConstraint_get_limits(self._cons)
+    
+    @enable_limits.setter
+    def enable_limits(self, enable_limits: bool) -> None:
+        lib.nvHingeConstraint_set_limits(self._cons, enable_limits)
+
+    @property
+    def upper_limit(self) -> float:
+        return lib.nvHingeConstraint_get_upper_limit(self._cons)
+    
+    @upper_limit.setter
+    def upper_limit(self, upper_limit: float) -> None:
+        lib.nvHingeConstraint_set_upper_limit(self._cons, upper_limit)
+
+    @property
+    def lower_limit(self) -> float:
+        return lib.nvHingeConstraint_get_lower_limit(self._cons)
+    
+    @lower_limit.setter
+    def lower_limit(self, lower_limit: float) -> None:
+        lib.nvHingeConstraint_set_lower_limit(self._cons, lower_limit)
+
+    @property
+    def max_force(self) -> float:
+        return lib.nvHingeConstraint_get_max_force(self._cons)
+    
+    @max_force.setter
+    def max_force(self, max_force: float) -> None:
+        lib.nvHingeConstraint_set_max_force(self._cons, max_force)
