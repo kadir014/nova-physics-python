e4c9d9582d51d381fe5756a9d20c33afc0e27f5a
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2026-04-13 23:21:54 +0300

    enter 0.2.1

--- nova/__init__.py
+++ nova/__init__.py
@@ -12,7 +12,7 @@ ffi = _nova.ffi
 
 __version_major__ = 0
 __version_minor__ = 2
-__version_patch__ = 0
+__version_patch__ = 1
 __version__ = f"{__version_major__}.{__version_minor__}.{__version_patch__}"
 
 
